3dfe6a4f89288068c13ed28c9b5908d00c6954bd
Lindsay Wilson <lindsay@programternimac.lan>
2019-12-16 09:17:02 -0600

    node commit init

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request= require('request')
+
+
+
+const forecast=(latitude, longitude, callback)=> {
+    const url= 'https://api.darksky.net/forecast/a220b4be32f121279c0d88449df4e8de/' + latitude + ',' + longitude
+
+
+request({url, json: true}, (error, {body})=>{
+    if(error){
+        callback('unable to connect to weather services', undefined)
+    } else if(body.error) {
+        callback('invalid location', undefined)
+    } else {
+        callback(undefined, 
+            body.daily.data[0].summary + ' it is currently ' + body.currently.temperature + ' degrees and there is a ' + body.currently.precipProbability + '% chance of rain.')
+    }
+  })
+}
+
+
+module.exports = forecast
